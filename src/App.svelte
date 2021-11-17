3a15af27b4c8da62ae5db4131589908ca4d42abd
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-17 15:38:49 +0530

    sample code for referencd

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,17 @@
 <script>
-	export let name;
+  import AudioPlayer, { stopAll } from './AudioPlayer.svelte'
+
+  let audioTracks = [
+    'https://sveltejs.github.io/assets/music/strauss.mp3',
+    'https://sveltejs.github.io/assets/music/holst.mp3',
+    'https://sveltejs.github.io/assets/music/satie.mp3'
+  ]
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<button on:click={stopAll}>
+  Stop all!
+</button>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+{#each audioTracks as src}
+  <AudioPlayer {src} />
+{/each}
